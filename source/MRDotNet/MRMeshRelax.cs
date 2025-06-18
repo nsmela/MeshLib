3bee753fe8beb2216884aa3d2606d25e749261dc
NathanSmela <nsmela@hotmail.com>
2025-06-18 08:13:40 -0700

    attempted to implement, but MRProgressCallback isn't exposed yet. saving progress until further work is done.

---
+++ source/MRDotNet/MRMeshRelax.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace MR 
+{
+    public partial class DotNet
+    {
+
+        public struct RelaxParameters
+        {
+            public int iterations = 1; // number of iterations
+            public VertBitSet region = new VertBitSet(); // region to relax
+            public float force = 0.5f; // speed of relaxing. Typical values [0.0f, 0.5f]
+            public bool limitNearInitial = false; // if true, limits maximal displacement of each point
+            public float maxInitialDist = 1.0f; // maximum distance between a point and its position before relaxation
+
+            public RelaxParameters() { }
+        }
+
+
+        public class MeshRelax
+        {
+            [StructLayout(LayoutKind.Sequential)]
+            internal struct MRRelaxParameters
+            {
+                public int iterations = 1; // number of iterations
+                public VertBitSet region = new VertBitSet(); // region to relax
+                public float force = 0.5f; // speed of relaxing. Typical values [0.0f, 0.5f]
+                public bool limitNearInitial = false; // if true, limits maximal displacement of each point
+                public float maxInitialDist = 1.0f; // maximum distance between a point and its position before relaxation
+
+                public MRRelaxParameters() { }
+            }
+
+            [DllImport("MRMesh", CharSet = CharSet.Ansi)]
+            private static extern bool mrRelax(ref Mesh mesh, ref MRRelaxParameters parameters, ref IntPtr progressCallback);
+
+            public static Mesh RelaxMesh(Mesh mesh, RelaxParameters parameters)
+            {
+                // callback pointer
+                IntPtr progressCallback = IntPtr.Zero;
+
+                // convert public struct to internal struct
+                MRRelaxParameters mrParameters = new MRRelaxParameters();
+                mrParameters.iterations = parameters.iterations;
+                mrParameters.region = parameters.region;
+                mrParameters.force = parameters.force;
+                mrParameters.limitNearInitial = parameters.limitNearInitial;
+                mrParameters.maxInitialDist = parameters.maxInitialDist;
+
+                /// applies given number of relaxation iterations to the whole mesh ( or some region if it is specified )
+                /// \return true if was finished successfully, false if was interrupted by progress callback
+                var finished = mrRelax(ref mesh, ref mrParameters, ref progressCallback);
+
+                return mesh;
+            }
+        }
+
+    }
+}
---
+++ source/MRDotNetTest/MeshRelaxTests.cs
@@ -0,0 +1,38 @@
+﻿using MR;
+using NUnit.Framework;
+using System;
+using static MR.DotNet;
+
+namespace MR.Test
+{
+    [TestFixture]
+    internal class MeshRelaxTests
+    {
+        [Test]
+        public void RelaxMesh_ShouldNotThrow_AndReturnSameMeshInstance()
+        {
+            // Arrange
+            var size = Vector3f.Diagonal(1.0f);
+            var baseCoords = new Vector3f();
+            var mesh = Mesh.MakeCube(size, baseCoords);
+            var originalPoints = mesh.Points;
+            var parameters = new RelaxParameters
+            {
+                iterations = 2,
+                force = 0.3f,
+                limitNearInitial = true,
+                maxInitialDist = 0.5f
+            };
+
+            // Act
+            var resultMesh = DotNet.MeshRelax.RelaxMesh(mesh, parameters);
+
+            // Assert
+            Assert.That(mesh, Is.EqualTo(resultMesh), "RelaxMesh should return the same mesh instance.");
+            Assert.That(resultMesh.Points is not null, "Mesh points should not be null after relaxation.");
+            // Optionally: check that points have changed (relaxation effect)
+            // This is a weak check, as actual values depend on native implementation
+            Assert.That(originalPoints.Count == resultMesh.Points.Count, "Point count should remain the same after relaxation.");
+        }
+    }
+}
