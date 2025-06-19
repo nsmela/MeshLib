f0a16143b51c9fb21738f4978cfebef6eb3c1a75
NathanSmela <nsmela@hotmail.com>
2025-06-19 09:23:29 -0700

    initial. gett errors

---
+++ source/MRDotNet/MeshExtrude.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using static MR.DotNet;
+
+namespace MR
+{
+    public partial class DotNet
+    {
+        /// <summary>
+        /// holds together settings for makeDegenerateBandAroundRegion
+        /// </summary>
+        public struct MakeDegenerateBandAroundRegionParams
+        {
+            public FaceBitSet? outnewFaces = new FaceBitSet();
+            public UndirectedEdgeBitSet? outExtrudedEdges = new UndirectedEdgeBitSet();
+            public float? maxEdgeLength = 1.0f;
+            //public MRVertMap? new2OldMap = new MRVertMap();
+
+            public MakeDegenerateBandAroundRegionParams() { }
+        }
+
+        public class MeshExtrude
+        {
+
+            [StructLayout(LayoutKind.Sequential)]
+            internal struct MRMakeDegenerateBandAroundRegionParams
+            {
+                public IntPtr outnewFaces = IntPtr.Zero;
+                public IntPtr outExtrudedEdges = IntPtr.Zero;
+                public IntPtr edgeLength = IntPtr.Zero;
+                public IntPtr new2OldMap = IntPtr.Zero;
+
+                public MRMakeDegenerateBandAroundRegionParams() { }
+            }
+
+            [DllImport("MRMeshC", CharSet = CharSet.Ansi)]
+            private static extern void mrMakeDegenerateBandAroundRegion(ref Mesh mesh, FaceBitSet region, ref MRMakeDegenerateBandAroundRegionParams parameters );
+
+            public static Mesh Extrude(Mesh mesh, ref FaceBitSet region, ref MakeDegenerateBandAroundRegionParams refParams)
+            {
+                if (mesh == null) throw new ArgumentNullException(nameof(mesh));
+                if (region.Size() == 0) throw new ArgumentException("Region must contain at least one face.", nameof(region));
+
+                MRMakeDegenerateBandAroundRegionParams parameters = new();
+                // TODO parameters.outnewFaces = refParams.outnewFaces?.bs_ ?? IntPtr.Zero;
+                // TODO parameters.outExtrudedEdges = refParams.outExtrudedEdges?.bs_ ?? IntPtr.Zero;
+                // TODO float
+                // TODO VertMap
+                mrMakeDegenerateBandAroundRegion(ref mesh, region, ref parameters);
+                return mesh; // Return the extruded mesh
+            }
+        }
+
+
+    }
+}
---
+++ source/MRDotNetTest/MeshExtrudeTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using NUnit.Framework;
+using static MR.DotNet;
+
+namespace MR.Test
+{
+    [TestFixture]
+    internal class MeshExtrudeTests
+    {
+        [Test]
+        public void TestMeshExtrude()
+        {
+            Assert.DoesNotThrow(() =>
+            {
+                var mesh = Mesh.MakeCube(Vector3f.Diagonal(1), Vector3f.Diagonal(-0.5f));
+                var count = mesh.ValidPoints.Count();
+
+                FaceBitSet region = new();
+                region.AutoResizeSet(3, false);
+                region.Set(0, true);
+                region.Set(1, true);
+
+                MakeDegenerateBandAroundRegionParams parameters = new();
+                mesh = MeshExtrude.Extrude(mesh, ref region, ref parameters);
+                Assert.That(mesh is not null, "Mesh extrusion failed to produce a valid mesh.");
+                mesh.Dispose();
+            });
+        }
+    }
+}
